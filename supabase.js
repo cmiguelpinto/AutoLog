a13deeef97c5b728d05b39ac08fd25d794efaae1
cmiguelpinto <50466103+cmiguelpinto@users.noreply.github.com>
2026-05-17 23:28:18 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://hbwgxesnylsejvhjhonl.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_nwooXkDdVEyUtb5JZp8hcg_RvL8xSJZ";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
