8b52c2e68a2b684ae79e5d23cb5114554ce60fbe
cmiguelpinto <50466103+cmiguelpinto@users.noreply.github.com>
2026-05-17 23:29:02 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://hbwgxesnylsejvhjhonl.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_nwooXkDdVEyUtb5JZp8hcg_RvL8xSJZ";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhid2d4ZXNueWxzZWp2aGpob25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDY2NjQsImV4cCI6MjA5NDYyMjY2NH0.pK7gxdxSQtE-Mfx8BjWL0ZZodQM3yMwFgbcCImVWOpY";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
